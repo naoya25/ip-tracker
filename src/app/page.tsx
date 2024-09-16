4fed6c563e17919dba3fd0d63999df9916748f1c
naoya25 <onaoya2002@gmail.com>
2024-09-17 02:12:56 +0900

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="m-0 p-0 relative w-full h-[100vh]">
+    <div className="m-0 p-0 relative w-[100vw] h-[100vh]">
       <Image src="/pay_img.png" alt="paypay" fill className="object-contain" />
     </div>
   );
